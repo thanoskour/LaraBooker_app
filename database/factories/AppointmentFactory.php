3b393721ce37224305140ad74c4f54b6726af813
thanoskour <cs04392@uoi.gr>
2024-01-01 21:08:37 +0200

    Data Seeders,make the seeders for making the appointments and the users

---
+++ database/factories/AppointmentFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Appointment;
+use App\Models\User;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Appointment>
+ */
+class AppointmentFactory extends Factory
+{
+     /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Appointment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => User::inRandomOrder()->first()->id ?? User::factory(), // This will create a User for each Appointment
+            'date' => $this->faker->dateTimeBetween('+1 week', '+1 month'),
+            'user_email' => $this->faker->email,
+            // Add other fields as necessary
+        ];
+    }
+}
---
+++ database/seeders/AppointmentsTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Appointment;
+
+
+class AppointmentsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Appointment::factory(10)->create();
+    }
+}
